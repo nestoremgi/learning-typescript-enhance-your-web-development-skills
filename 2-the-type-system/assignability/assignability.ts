222d3ddf97efebdb28634d8fc75794fc3b753894
nestor <nestor.dev.en@gmail.com>
2023-10-19 21:26:47 -0600

    add assignability

---
+++ 2-the-type-system/assignability/assignability.ts
@@ -0,0 +1,7 @@
+//Ts  checks first assignation
+//and after check if type would be same for next
+let firstName = "Carole";
+firstName = "Joan";
+
+let lastName = "King";
+//lastName = true; El tipo 'boolean' no se puede asignar al tipo 'string'.
