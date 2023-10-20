cd8c25b0b9e4b6f2c882788c44587ee9ad9c63a3
nestor <nestor.dev.en@gmail.com>
2023-10-19 21:28:06 -0600

    add anotations to demostre asignability

---
+++ 2-the-type-system/type-anotations/anotations.ts
@@ -0,0 +1,16 @@
+let rocker;
+
+rocker = "Joan Jett";
+rocker.toUpperCase();
+
+rocker = 19.58;
+rocker.toPrecision();
+
+rocker.toUpperCase();
+
+let rockandroller: string;
+
+rockandroller = "Joan Jett";
+
+//rockandroller = 19.58; El tipo 'number' no se puede asignar al tipo 'string'.
+
